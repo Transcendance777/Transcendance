77e5d4b3fb2f8dd2c465f34b9f615d4300fdb8ff
mariosrb <mario-2000@hotmail.fr>
2026-05-20 15:37:42 +0200

    feat: Add .env.example and script to generate SSL certificates

---
+++ scripts/generate_certs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Dossier de stockage certificats
+CERT_DIR="./nginx/certs"
+
+mkdir -p $CERT_DIR
+
+# Générer le certificat sans poser de questions (grâce à -subj)
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+  -keyout "$CERT_DIR/key.key" \
+  -out "$CERT_DIR/cert.crt" \
+  -subj "/C=FR/ST=Paris/L=Paris/O=GameRev/CN=localhost"
+
+chmod 600 "$CERT_DIR/key.key" "$CERT_DIR/cert.crt"
+
+echo "Certificats SSL générés avec succès dans $CERT_DIR !"
