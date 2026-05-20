befec47fa7b12584ff3673406b5e6673944c4db8
Rydom <ryddev18@gmail.com>
2026-05-20 16:47:48 +0200

    composant background à finir, ajouter les props pour rendre les rendre generique

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,34 +1,60 @@
 import axios from 'axios'
 
+const NavBar = () => {
+	return (
+		<div style={{ backgroundColor: "black"}}>
+			<nav className="navbar">
+				<h1 className="GameRev">GAME REV</h1>
+				<input type="search" placeholder="Rechercher un jeu..." />
+				<a href="" className="texte">Games</a>
+				<a href="" className="texte">Reviews</a>
+				<a href="" className="texte">Profil</a>
+			</nav>
+		</div>
+	)
+}
+
+const Background = () =>{
+	return (
+		<div className="backgroundImage">
+			<div className="backgroundOverlay"></div>
+		</div>
+	)
+}
+
+const InscriptionForm = () => {
+	return (
+		<div className="formBackground">
+			<form action="" method="POST">
+				<p className="emailMessage texte">Saissez votre adressse email :</p>
+				<input className="emailArea" type="email" placeholder="Exemple: TungTungSahur@gmail.com" />
+				<br />
+				<p className="passwordMessage texte">Saissez votre mot de passe :</p>
+				<input className="passwordArea" type="password" />
+				<br /><br />
+				<input type="submit" />
+			</form>
+		</div>
+	)
+}
+
 const App = () => {
 
-	const titreStyle = {
-
-	}
 	return (
 		<>
-		<div style={{ backgroundColor: "black", minHeight: "100vh" }}>
-		<nav className="navbar">
-		<h1 className="GameRev">GAME REV</h1>
-		<input type="search" placeholder="Rechercher un jeu..." />
-		<a href="" className="texte">Games</a>
-		<a href="" className="texte">Reviews</a>
-		<a href="" className="texte">Profil</a>
-		</nav>
-		<div className="hero">
-		<div className="formBackground">
-		<form action="" method="POST">
-		<p className="emailMessage texte">Saissez votre adressse email :</p>
-		<input className="emailArea" type="email" placeholder="Exemple: TungTungSahur@gmail.com"/>
-		<br />
-		<p className="passwordMessage texte">Saissez votre mot de passe :</p>
-		<input className="passwordArea" type="password"/>
-		<br /><br />
-		<input type="submit"/>
-		</form>
-		</div>
-		</div>
-		</div>
+		<NavBar />
+		<Background />
+				<div className="formBackground">
+					<form action="" method="POST">
+					<p className="emailMessage texte">Saissez votre adressse email :</p>
+					<input className="emailArea" type="email" placeholder="Exemple: TungTungSahur@gmail.com"/>
+					<br />
+					<p className="passwordMessage texte">Saissez votre mot de passe :</p>
+					<input className="passwordArea" type="password"/>
+					<br /><br />
+					<input type="submit"/>
+					</form>
+			</div>
 		</>
 	)
 }
