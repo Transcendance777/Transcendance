a1cf9378899f308e45c0e806c5c1bc7c9099793c
rmiah <rmiah@DESKTOP-ET61QVA.localdomain>
2026-05-21 02:05:18 +0200

    Structuration du code en composants, code plus lisible plus clair, visuel de la page de test pour connection front/back temriné, il reste plus qu'à ajouter les fonctions d'interraction avec la saisie d'email et de MDP et avec le back, call API, fetch

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,22 +2,31 @@ import axios from 'axios'
 
 const NavBar = () => {
 	return (
-		<div style={{ backgroundColor: "black"}}>
+		<div style={{ backgroundColor: "black" }}>
 			<nav className="navbar">
-				<h1 className="GameRev">GAME REV</h1>
-				<input type="search" placeholder="Rechercher un jeu..." />
-				<a href="" className="texte">Games</a>
-				<a href="" className="texte">Reviews</a>
-				<a href="" className="texte">Profil</a>
+				<div className="navbar-left">
+					<a href="" className="texte">Games</a>
+					<a href="" className="texte">Reviews</a>
+					<a href="" className="texte">Friends</a>
+					<a href="" className="texte" style={{ fontSize: "35px"}}>+</a>
+				</div>
+				<div className="navbar-center">
+					<h1 className="GameRev">GAME REV</h1>
+				</div>
+				<div className="navbar-right">
+					<input type="search" placeholder="Rechercher un jeu..." />
+					<a href="" className="texte">Profil</a>
+				</div>
 			</nav>
 		</div>
 	)
 }
 
-const Background = () =>{
+const Background = ({children, ...rest}) =>{
 	return (
-		<div className="backgroundImage">
+		<div className="backgroundImage" {...rest}>
 			<div className="backgroundOverlay"></div>
+			{children}
 		</div>
 	)
 }
@@ -43,18 +52,9 @@ const App = () => {
 	return (
 		<>
 		<NavBar />
-		<Background />
-				<div className="formBackground">
-					<form action="" method="POST">
-					<p className="emailMessage texte">Saissez votre adressse email :</p>
-					<input className="emailArea" type="email" placeholder="Exemple: TungTungSahur@gmail.com"/>
-					<br />
-					<p className="passwordMessage texte">Saissez votre mot de passe :</p>
-					<input className="passwordArea" type="password"/>
-					<br /><br />
-					<input type="submit"/>
-					</form>
-			</div>
+		<Background style={{ justifyContent: "center", alignItems: "center" }}>
+			<InscriptionForm />
+		</Background>
 		</>
 	)
 }
