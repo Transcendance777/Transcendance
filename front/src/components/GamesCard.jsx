784f4e3f96a8f5107deff7bd8b46fc50581a94ac
rmiah <rmiah@DESKTOP-ET61QVA.localdomain>
2026-06-08 21:05:13 +0200

    fix carousal responsive

--- front/src/components/GamesCard.jsx
+++ front/src/components/GamesCard.jsx
@@ -1,8 +1,11 @@
 import '../styles/GamesCard.css'
 
-const GamesCard = ({ game }) => {
+const GamesCard = ({ game, visibleCount }) => {
 	return (
-		<div className="games-card">
+		<div
+			className="games-card"
+			style={{ width: `calc((100% - ${15 * (visibleCount - 1)}px) / ${visibleCount})` }}
+		>
 			<img src={game.image} alt={game.title} className="games-card-img" />
 			<p className="games-card-title">{game.title}</p>
 		</div>
