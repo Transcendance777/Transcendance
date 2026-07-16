69f6fd040c4fe5481e1ad694b5bdf9c8d2dc88e3
zuccc23 <daniyaahmane@gmail.com>
2026-07-16 16:19:56 +0200

    modified max username length to 8 characters

--- apps/front/src/utils/validation.js
+++ apps/front/src/utils/validation.js
@@ -1,7 +1,7 @@
 export const MIN_PASSWORD_LENGTH = 6
 export const MAX_PASSWORD_LENGTH = 128
 export const MIN_USERNAME_LENGTH = 4
-export const MAX_USERNAME_LENGTH = 15
+export const MAX_USERNAME_LENGTH = 8
 export const MAX_SEARCH_QUERY_LENGTH = 100
 export const MAX_REVIEW_TEXT_LENGTH = 500
 export const MAX_COMMENT_TEXT_LENGTH = 200
